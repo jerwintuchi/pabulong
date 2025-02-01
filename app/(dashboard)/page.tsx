d008783d1cedf6e044bcd6dd18fda296348530ea
jerwintuchi <jerwin.tuchi3@gmail.com>
2025-02-02 00:04:30 +0800

    ftr: added middleware for secretpages

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -13,27 +13,29 @@ export default async function HomePage() {
     const isAuthenticated = await getUser();
     console.log("isAuthenticated", isAuthenticated)
     // If not authenticated, render Home component
-    if (!isAuthenticated) {
-        return <Home />;
+    if (isAuthenticated) {
+        // If authenticated, fetch user data
+        const user = await getUser(); // Assuming getUser() returns the full user data
+        const username = await getUserName(); // Assuming user.id is available
+        const secretMessage = await getSecretMessage();
+        const friends = await getUserFriends(); // Assuming `user.friends` is an array
+        const pendingRequests = await getPendingFriendRequests(); // Assuming `user.pendingRequests` is an array
+
+        // Combine all the data into a single object of type UserType
+        const userData: UserType = {
+            user: user,
+            username: username,
+            secretMessage: secretMessage,
+            friends: friends,
+            pendingRequests: pendingRequests,
+        };
+
+        // Render the HomePageClient with the user data as props
+        return <HomePageClient user={userData} />;
     }
-    // If authenticated, fetch user data
-    const user = await getUser(); // Assuming getUser() returns the full user data
-    const username = await getUserName(); // Assuming user.id is available
-    const secretMessage = await getSecretMessage();
-    const friends = await getUserFriends(); // Assuming `user.friends` is an array
-    const pendingRequests = await getPendingFriendRequests(); // Assuming `user.pendingRequests` is an array
 
-    // Combine all the data into a single object of type UserType
-    const userData: UserType = {
-        user: user,
-        username: username,
-        secretMessage: secretMessage,
-        friends: friends,
-        pendingRequests: pendingRequests,
-    };
-
-    // Render the HomePageClient with the user data as props
-    return <HomePageClient user={userData} />;
+    // If not authenticated, render Home component
+    return <Home />
 
 }
 
