afb039b1e984941a696ca8390f13bf673daff878
jerwintuchi <jerwin.tuchi3@gmail.com>
2025-02-03 22:36:57 +0800

    removed Hero component

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,12 +1,11 @@
 
-import Hero from "@/components/Hero";
+
 import FeatureSection from "../app/public/_components/FeatureSection";
 import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   return (
     <>
-      <Hero />
       <main className="flex-1 flex flex-col gap-12 px-4 py-12  dark:bg-black dark:text-white">
 
         {/* Feature Section */}
